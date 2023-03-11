affe067822750df050d836c70aca11be3ee7f70a
PC <relrysalves@gmail.com>
2023-03-11 11:21:17 -0300

    getts and settes das class

--- app/src/main/java/devandroid/alves/applistacurso2023/model/Pessoa.java
+++ app/src/main/java/devandroid/alves/applistacurso2023/model/Pessoa.java
@@ -5,12 +5,47 @@ package devandroid.alves.applistacurso2023.model;
 public class Pessoa {
 
     //Contrutor
+    // Atributos - Objetos - Molde - modelo - template
+    // Métodos de Acesso - Getts and setts
 
-    public Pessoa(){
+    private String primeiroNome;  //Primeiro Nome
+    private String sobreNome;
+    private String telefoneContato;
+    private String cursoDesejado;
 
 
+    public Pessoa(){}
 
+    public String getPrimeiroNome() {
+        return primeiroNome;
     }
 
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
 }
 
--- app/src/main/java/devandroid/alves/applistacurso2023/view/MainActivity.java
+++ app/src/main/java/devandroid/alves/applistacurso2023/view/MainActivity.java
@@ -11,6 +11,7 @@ import devandroid.alves.applistacurso2023.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +19,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+        //Atribuir conteúdo Dados ,valpres para objetos
+        pessoa.setPrimeiroNome("Relrys");
+        pessoa.setSobreNome("Alves");
+        pessoa.setTelefoneContato("85 8597854");
+        pessoa.setCursoDesejado("Android");
+
+        outraPessoa =  new Pessoa();
+        outraPessoa.setPrimeiroNome("Hayna");
+        outraPessoa.setSobreNome("Alves");
+        outraPessoa.setTelefoneContato("85486514156");
+        outraPessoa.setCursoDesejado("java");
+
+
+        int parada = 0;
 
     }
 }
